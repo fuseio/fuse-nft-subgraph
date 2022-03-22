bfed352c2bfd0dcbe0fc539824a5ae1cea229068
codingsh <codingsh@pm.me>
2022-03-22 22:25:40 +0000

    feat(fuse-nft): get All nft data

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { BigInt} from '@graphprotocol/graph-ts'
+import { Address } from "@graphprotocol/graph-ts";
 
 export let IPFS_SCHEME = 'ipfs://'
 
@@ -9,8 +10,6 @@ export let BASE_IPFS_URL = 'https://ipfs.io/ipfs/'
 export let DWEB_IPFS_URL = 'https://dweb.link/ipfs/'
 
 export let COZY_ASSET_URL = 'https://bafybeid64tyd25ovegjakaq6qyjyo332qzeqj3md6lygqgjg6aj5u5gybi.ipfs.dweb.link/'
-
-export const COZY_ADDRESS = "0x32319834d90323127988E4e2DC7b2162d4262904"
 
 export function getURIScheme (input: string): string {
     return input.split(':')[0]
@@ -29,3 +28,9 @@ export function getDwebURL(ipfsURI: string): string {
     let ipfsURL = DWEB_IPFS_URL + getIpfsPath(ipfsURI)
     return ipfsURL
 }
+
+export let COZY_ADDRESS = Address.fromString(
+    "0x32319834d90323127988E4e2DC7b2162d4262904"
+  );
+
+export let ZERO_ADDRESS_STRING = '0x0000000000000000000000000000000000000000';
