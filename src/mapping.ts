226f1626212156bdb66505a0049c352beb392f8d
mul53 <mulengabowa53@gmail.com>
2022-12-12 09:18:25 +0200

    bitazza fix

--- src/mapping.ts
+++ src/mapping.ts
@@ -31,47 +31,57 @@ export function handleTransfer(event: Transfer): void {
   }
 
   let data = ipfs.cat(contentPath)
-  if (!data) return
-
-  let jsonResult = json.try_fromBytes(data!)
-  if (jsonResult.isError) return
-
-  let value = jsonResult.value.toObject()
-  if (data != null) {
-    let name = value.get('name')
-    if (name != null && name.kind == JSONValueKind.STRING) {
-      collectible.name = name.toString()
-    } else {
-      return
-    }
-
-    let description = value.get('description')
-    if (description != null && description.kind == JSONValueKind.STRING) {
-      collectible.description = description.toString()
-    } else {
-      return
-    }
-
-    let image = value.get('image')
-    if (image != null && image.kind == JSONValueKind.STRING) {
-      let imageStr = image.toString()
-      if (imageStr.includes(IPFS_SCHEME)) {
-        imageStr = getIpfsURL(imageStr)
+  if (data) {
+    let jsonResult = json.try_fromBytes(data!)
+    if (jsonResult.isError) return
+  
+    let value = jsonResult.value.toObject()
+    if (data != null) {
+      let name = value.get('name')
+      if (name != null && name.kind == JSONValueKind.STRING) {
+        collectible.name = name.toString()
+      } else {
+        return
       }
-      collectible.imageURL = imageStr
-    } else {
-      return
+  
+      let description = value.get('description')
+      if (description != null && description.kind == JSONValueKind.STRING) {
+        collectible.description = description.toString()
+      } else {
+        return
+      }
+  
+      let image = value.get('image')
+      if (image != null && image.kind == JSONValueKind.STRING) {
+        let imageStr = image.toString()
+        if (imageStr.includes(IPFS_SCHEME)) {
+          imageStr = getIpfsURL(imageStr)
+        }
+        collectible.imageURL = imageStr
+      } else {
+        return
+      }
     }
-  }
-
-  let name = erc721Token.try_name()
-  if (!name.reverted) {
-    collectible.collectionName = name.value
-  }
-
-  let symbol = erc721Token.try_symbol()
-  if (!symbol.reverted) {
-    collectible.collectionSymbol = symbol.value
+  
+    let name = erc721Token.try_name()
+    if (!name.reverted) {
+      collectible.collectionName = name.value
+    }
+  
+    let symbol = erc721Token.try_symbol()
+    if (!symbol.reverted) {
+      collectible.collectionSymbol = symbol.value
+    }
+  } else {
+    if (event.address.toHexString() == '0x3f82395600296eef2e7022673bce036d3b040cf6') {
+      collectible.name = 'FreedomCup'
+      collectible.description = 'Celebrate the World Cup 2022 with Freedom Community by joining our Freedom Cup Campaign. Guess the result, get the NFT, and recieve rewards from the prize pool!'
+      collectible.collectionName = 'FreedomCup'
+      collectible.collectionSymbol = 'FDCUP'
+      collectible.imageURL = 'https://assets.bitazza.com/images/freedom-cup.png'
+    } else {
+      return 
+    }
   }
 
   collectible.owner = event.params.to.toHexString()
